cf5955a32352bc5655f4fb665ee576e288d309e2
DmitriKo <dmitrikozhevin@gmail.com>
2021-12-02 12:39:12 +0200

    game session init version

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -1,13 +1,23 @@
 package server
 
 import (
+	"context"
 	"io/ioutil"
+	"math/rand"
 	"path/filepath"
 	"strings"
+	"time"
+)
+
+const (
+	NotStarted = iota
+	Started
+	Ended
 )
 
 type GameConfig struct {
 	ImagesPath string
+	TickPeriod int //miliseconds
 }
 
 type Image struct {
@@ -18,6 +28,22 @@ type Image struct {
 type Game struct {
 	conf   GameConfig
 	Images []*Image
+}
+
+type GameSession struct {
+	ctx          context.Context
+	TickDuration time.Duration
+	Image        *Image
+	Status       int
+	Done         chan bool
+	In           <-chan Guess
+	Out          chan<- string
+	CurrentLine  int
+}
+
+type Guess struct {
+	UserName  string
+	ImageName string
 }
 
 func NewGame(conf GameConfig) (*Game, error) {
@@ -48,4 +74,36 @@ func (g *Game) LoadImages() error {
 		g.Images = append(g.Images, img)
 	}
 	return nil
+}
+
+func (g *Game) NewSession(ctx context.Context, in chan Guess, out chan string) (*GameSession, error) {
+	rand.Seed(time.Now().Unix())
+	s := &GameSession{
+		ctx:          ctx,
+		TickDuration: time.Duration(g.conf.TickPeriod),
+		Image:        g.Images[rand.Intn(len(g.Images))],
+		Done:         make(chan bool),
+		In:           in,
+		Out:          out,
+	}
+	return s, nil
+}
+
+func (s *GameSession) Start() {
+	ticker := time.NewTicker(s.TickDuration)
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		case <-ticker.C:
+			if len(s.Image.Lines) <= s.CurrentLine {
+				s.Out <- "Nobody wins."
+				s.Done <- true
+			} else {
+				s.Out <- s.Image.Lines[s.CurrentLine]
+				s.CurrentLine++
+			}
+
+		}
+	}
 }
--- pkg/server/main_test.go
+++ pkg/server/main_test.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,13 +11,42 @@ import (
 func TestNewGame(t *testing.T) {
 	conf := GameConfig{
 		ImagesPath: "test_data/asciiImages",
+		TickPeriod: 1,
 	}
 	g, err := NewGame(conf)
 	assert.Nil(t, err)
 	assert.Equal(t, 4, len(g.Images))
 	assert.Equal(t, "camel", g.Images[0].Name)
 
-	badConf := GameConfig{"foo"}
+	badConf := GameConfig{"foo", 1}
 	_, err = NewGame(badConf)
 	assert.NotNil(t, err)
+}
+
+func TestNewGameSession(t *testing.T) {
+	conf := GameConfig{
+		ImagesPath: "test_data/asciiImages",
+		TickPeriod: 1,
+	}
+	g, err := NewGame(conf)
+	assert.Nil(t, err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Millisecond*500))
+	defer cancel()
+	guessChan := make(chan Guess)
+	outChan := make(chan string)
+	s, err := g.NewSession(ctx, guessChan, outChan)
+	assert.Nil(t, err)
+	go func() {
+		for {
+			<-outChan
+		}
+	}()
+	go s.Start()
+	select {
+	case <-ctx.Done():
+		assert.Nil(t, ctx.Err())
+	case done := <-s.Done:
+		assert.True(t, done)
+	}
+
 }
